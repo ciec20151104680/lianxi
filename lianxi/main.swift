4fe91a2a71c3b062cf5f796cb5569a2937305493
ciec20151104680 <cxxxe4680@Mac28.local>
2017-02-27 11:42:11 +0800

    0227practice

---
+++ lianxi/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  lianxi
+//
+//  Created by cxxxe4680 on 2017/2/27.
+//  Copyright © 2017年 cxxxe4680. All rights reserved.
+//
+
+// Integer:1/10/100/1000
+//Double:3.14/5.23
+
+/*var i=1,sum=0;
+ while i<=100
+{
+   sum=sum+i;
+   i=i+1;
+ }
+
+
+print(sum)
+ */
